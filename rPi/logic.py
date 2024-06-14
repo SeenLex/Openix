bb5f970419ca3148d22626466e3ccf6133eac762
Denis <draghicidenis@yahoo.com>
2024-06-14 14:55:28 +0300

    -rPi

--- rPi/logic.py
+++
@@ -1,9 +0,0 @@
-def check_license_plate_exists(license_plates, detected_text):
-    return detected_text in license_plates
-
-
-def show_check_status(license_plate_exists):
-    if license_plate_exists:
-        print("License plate exists in the database.")
-    else:
-        print("License plate does not exist in the database.")
